5e1fc15d7e17ad3262bea8fe534f37c946d6b54c
chardi90 <chardi90@hotmail.com>
2025-05-14 14:21:01 +0100

    Added footer, fixed JS toggles, added footer CSS toggle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 function handleClick(event) {
   event.preventDefault();
   alert("Hooray!");
-  let button = document.querySelector("button");
+  let button = document.querySelector("#special-button");
   button.style.background = "magenta";
   button.innerHTML = "Oops";
-  let heading = document.querySelector(".heading");
-  heading.classList = "highlight";
+  let heading = document.querySelector("#heading");
+  heading.classList.toggle("highlight");
   heading.innerHTML = "You dared!";
+  let footer = document.querySelector("#footer");
+  footer.classList.toggle("hidden");
 }
 
 let buttonElement = document.querySelector("#special-button");
