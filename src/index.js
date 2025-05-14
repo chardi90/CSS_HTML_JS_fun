8cd5b4f833ac5c9d3dc1e99f8981301b1fae1e56
chardi90 <chardi90@hotmail.com>
2025-05-14 13:47:37 +0100

    Added button color change to JS & styled h1 and fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 function handleClick(event) {
   event.preventDefault();
   alert("Hooray!");
+  let button = document.querySelector("button");
+  button.style.background = "magenta";
 }
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
 console.log(buttonElement.attributes);
-buttonElement.setAttributeNS("class", "danger");
+buttonElement.setAttribute("class", "danger");
