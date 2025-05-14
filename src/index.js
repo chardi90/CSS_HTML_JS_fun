fdd7b5c8b479a3436435906a8f93f6d6fee844b9
chardi90 <chardi90@hotmail.com>
2025-05-14 12:39:35 +0100

    Added simple handleClick alert to button in JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+function handleClick(event) {
+  event.preventDefault();
+  alert("Hooray!");
+}
+
 let buttonElement = document.querySelector("#special-button");
 console.log(buttonElement);
+buttonElement.addEventListener("click", handleClick);
